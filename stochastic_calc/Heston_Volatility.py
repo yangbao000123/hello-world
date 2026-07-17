d16f9ac4981799ddcceae2316387273cdc83b6ba
Yang Bao <93554411+yangbao328@users.noreply.github.com>
2026-07-17 09:17:01 -0500

    heston and implied volatility

--- stochastic_calc/Heston_Volatility.py
+++ stochastic_calc/Heston_Volatility.py
@@ -8,11 +8,15 @@ Created on Mon Jul 13 19:18:37 2026
 Heston Model, stochastic volatility
 a. St and vt, 13 July
 b. Monte-Carlo multi-path European call pricer, 14 July
-c. Implied volatility and plots
+c. Implied volatility smile by inverting B-S for different strikes
 """
-
-
+import sys
+import os
 import numpy as np
+from scipy.optimize import brentq
+
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', 'dynamic_hedging'))
+from the_engine import Options
 
 
 class Heston_Volatility:
@@ -72,6 +76,7 @@ class Heston_Volatility:
             
         return S #n_paths simulation, each with N (time)steps
     
+    
     def European_Call_MC_simu(self, K, n_paths):
         
         S_n_paths = self.St_heston_simu(n_paths)
@@ -96,6 +101,26 @@ class Heston_Volatility:
         
         return St
     
+    def implied_vol(self, K, sigma_low, sigma_high, n_paths):
+        '''
+        C_BS = S0N(d1) - e^(-rT) KN(d2)
+        Find sigma_BS such that C_BS = C_heston given S0, K, T and r
+        '''
+        r=self.r; T=self.T; S0=self.S0; T0=T-T
+    
+        #options = Options(K=self.K, sigma=self.v0, r=self.r, T=self.T)
+        #call_BS_low = Options(K=K, sigma=sigma_low, r=r, T=T).eu_call_BS(self.S0, 0)
+        #call_BS_high = Options(K=K, sigma=sigma_high, r=r, T=T).eu_call_BS(self.S0, 0)
+
+        call_heston = self.European_Call_MC_simu(K, n_paths)
+        #print(f"Call, Heston:{call_heston:.3f}, \
+        #        Call, sigma lower bound:{call_BS_low:.3f}, \
+        #        Call, sigma higher bound: {call_BS_high:.3f}")
+        
+        f = lambda sigma: Options(K=K, sigma=sigma, r=r, T=T).eu_call_BS(S0, T0) - call_heston
+        
+        return brentq(f, sigma_low, sigma_high)
+    
     def European_Call_MC(self, K, n_paths):
         
         St_MC = np.zeros(n_paths)
@@ -108,70 +133,38 @@ class Heston_Volatility:
             St_MC[n] = St
             payoff[n] = call_payoff
 
-            #rint(St, call_payoff)
+            #print(St, call_payoff)
         call_MC = np.exp(-self.r*self.T) * np.mean(payoff)
         
         return call_MC
     
 T = 1; N = 252; rho = -0.7; kappa = 0.1; theta = 0.12
-sigmav = 0.5; v0 = .3; r = 0.1; S0=100
+sigmav = 0.5; v0 = .3; r = 0.1; S0=100; K=90
 
 heston = Heston_Volatility(T=T, N=N, rho=rho, kappa=kappa, theta=theta, 
                            sigma_v = sigmav, v0 = v0, r = r, S0 = S0)
 St_single = heston.St_heston()
 european_C = heston.European_Call_MC(90, 1000)
-eu_MC = heston.European_Call_MC_simu(90, 10000)
-print(np.mean(heston.St_heston_simu(1000)))
-print(european_C)
-print(eu_MC)
+eu_MC = heston.European_Call_MC_simu(90, 1000)
+print(f"Single-path European Call:{np.mean(heston.St_heston_simu(1000)):.3f}")
+print(f"Monte-Carlo multi-path European Call:{eu_MC:.3f}")
+
+options = Options(K=K, sigma = v0**0.5, r = r, T = T)
+eu_BS = options.eu_call_BS(S0, 0)
+print(f"Black-Scholes European Call:{eu_BS:.3f}")
+
+f = lambda sigma: Options(K=K, sigma = sigma, r = r, T = T).eu_call_BS(S0, 0) - european_C
+brentq(f, 0.5, 1)
+
+strikes = np.arange(75, 125, 3)
+ivs = []
+for K in strikes:
+    ivs.append(heston.implied_vol(K, 0.05, 1, 1000))
+    
+
 #print(f"Risk-neutral expectation for St is {S0 * np.exp(r*T):.3f}")
 
 
-#%% Heston SDE simulation
-#Monte Carlo European Call Pricer
-
-
-
-n_p = 10000
-ST = np.zeros(n_p)
-for each in range(n_p):             
-       
-    St = np.zeros(N+1); 
-    Vt = np.zeros(N+1)
-    St[0] = S0; Vt[0] = v0
-    
-    for i in range(N):
-        Z1 = rng.normal(0, 1)       #N~(0,1)
-        Z2 = rng.normal(0, 1)       #CORRECTION from N~(0, dt**0.5); 
-                                    #error causes dWt~N(0, dt**2) with significantly smaller stdev
-        dWtS = (dt)**0.5*Z1         #N~(0,deltat**0.5)
-        dWtv = (dt)**0.5*(rho*Z1+(1-rho**2)**0.5*Z2)
-        Vt[i+1] = np.maximum(Vt[i] + kappa * (theta - Vt[i]) * dt + sigmav * Vt[i]**0.5 * dWtv,0)
-        St[i+1] = St[i] + r * St[i] * dt + Vt[i]**0.5 * St[i] * dWtS
-    ST[each] = St[-1]
-print("Mean S_T:", np.mean(ST)) 
-print(f"Risk-neutral expectation for St is {S0 * np.exp(r*T):.3f}")
-
-#%%
-#vt+i = vti + kappa(theta - vti)*deltat + sigmav (vti)**0.5 *dWtv; max(vt,0)
-#St+i = Sti + rSti*deltat + vti**0.5 Sti dWtS
-
-Z1 = np.random.normal(mu_dWtS, 1, N)
-Z2 = np.random.normal(mu_dWtS, 1, N)
-
-dWtS = (dt)**0.5*Z1
-dWtv = (dt)**0.5*(rho*Z1+(1-rho**2)**0.5*Z2)
-
-St = np.zeros(N+1); Vt = np.zeros(N+1)
-St[0] = S0; Vt[0] = v0
-
-for i in range(N):
-    
-    #volatility has floor 0, as being standard deviation**2
-    Vt[i+1] = max(Vt[i] + kappa * (theta - Vt[i]) * dt + sigmav * Vt[i]**0.5 * dWtv[i], 0)
-    St[i+1] = St[i] + r * St[i] * dt + Vt[i]**0.5 * St[i] * dWtS[i]
-
-print(St[-1])
 
 
 
@@ -185,30 +178,9 @@ print(St[-1])
 
 
 
-#NOTES and First Attempt
-import sys
-from pathlib import Path
 
-sys.path.append(str(Path(__file__).resolve().parent.parent))
 
-corr = 0.9
-vdWtS = sigma_dWts**2       #INCORRECT, dWt standard deviation == deltat**0.5
-vdWtv = 0.6                 #INCORRECT, dWt standard deviation == deltat**0.5
-#covrn = vdWtS*vdWtv*corr
-covMx = [[vdWtS**2, vdWtS*vdWtv*corr],[vdWtS*vdWtv*corr, vdWtv**2]]    #INCORRECT, design correlation matrix with rho
-L_chk = np.linalg.cholesky(covMx)
 
-#random shocks to dWtS and dWtv are not introduced and dWtS dimension is off
-dWtS = np.random.normal(loc=mu_dWtS, scale=sigma_dWts, size=(len(covMx),N)) 
-dWtv = L_chk@dWtS
-S0 = 100; mu_St = 0; sigma_St = 1.0; 
-#terminal St is not appropraite; 
-#intermediate St is needed to incorporate Heston SDE, with volatility at each step
-
-#vt+i = vti + kappa(theta - vti)*deltat + sigmav (vti)**0.5 *dWtv; max(vt,0)
-#St+i = Sti + rSti*deltat + vti**0.5 Sti dWtS
-
-St= 100 * np.exp(mu_St - 0.5 * sigma_St**2) * T + sigma_St*(T**0.5)*dWtS[0]
 
 
 
