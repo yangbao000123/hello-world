ece63330d8d8be1ef1ddbe756232fce04a19c78f
Yang Bao <93554411+yangbao328@users.noreply.github.com>
2026-07-26 13:43:50 -0500

    paired programming

---
+++ exercises/Programming.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 26 13:37:15 2026
+
+@author: tianyang
+
+paired programming exercise
+"""
+
+'''
+1. A deck has 50 red cards and 50 black cards. 
+   Card is drawn one at a time without replacement,
+   one may stop after any draw, and payoff is 2*#red/#drawn. 
+   If never stops, all 100 cards are drawn.
+   What is E[Payoff in $] if play optimally.
+'''
+
+
+
+'''
+2. Best candidate selection
+   Given the best candidate is at k, the best will be selected 
+   if secondary-best candidate prior to k is within the rejection window
+   reindex summation bounds to align with Harmonic Series
+
+'''
