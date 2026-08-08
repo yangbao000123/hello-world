a8a4655555fb7ba93816e2877ba6af17c4041cfd
Yang Bao <93554411+yangbao328@users.noreply.github.com>
2026-08-08 14:06:01 -0500

    momentum cleanup

--- simulated_project/momentum_S5.py
+++ simulated_project/momentum_S5.py
@@ -30,8 +30,14 @@ import numpy as np
 import pandas as pd
 import yfinance as yh 
 import matplotlib.pyplot as plt
-# S&P 500 constituent-daily price from yahoo finance; survivorship bias (note limitations)
-
+# Gather constituents and calcuate return
+'''
+S&P 500 constituent-daily price from yahoo finance; 
+Survivorship bias
+    - collection of constituents as of 07/31/2026 where it contains survived constituents only
+    - neglection of historical index rebalance 
+Compute daily returns and resampled to monthly cadence return
+'''
 url = 'https://raw.githubusercontent.com/datasets/s-and-p-500-companies/master/data/constituents.csv'
 sp500 = pd.read_csv(url)
 tickers = sp500['Symbol'].tolist()
@@ -44,58 +50,66 @@ close = data['Adj Close']
 
 data['Close'].isna().sum()
 
-# Compute daily log returns, monthly cadence return; momentum signal based off of monthly return
-'''by nature of month-end return, 
+price_monthly = data['Close'].resample('ME').last()
+return_monthl = price_monthly.pct_change()
+
+# Momentum signal for month t: growth from t-12 to t-1
+'''
+by nature of month-end return, 
 it means for portfolio simulation, position is entered at next-month beginning timestamp; 
 next-month return is to be earned/logged for performance that corresponds with current-month position entered, 
 assuming an enter-and-hold for monthly balance
 '''
-
-price_monthly = data['Close'].resample('ME').last()
-return_monthl = price_monthly.pct_change()
-
-# cumulative return index (starting at 1)
 total_return_idx = (1 + return_monthl).cumprod()
-# Momentum signal for month t: growth from t-12 to t-1
 momentum_signal = (total_return_idx.shift(1) / total_return_idx.shift(12)) - 1
 
-#2.
+# Alternative apporach to produce momentum signal
 price = data['Close'].resample('ME').last()
 momen = price.shift(1).diff(11)/price.shift(12)
-#3.
+#
 momentum_sgnl = (1+return_monthl).rolling(window=11).apply(np.prod, raw=True) - 1
 momentum_sgnl = momentum_sgnl.shift(1) # at t, the signal is of window t-1 to t-11
 
-
 # Vectorized backtest engine
-# daily rebalancing, quintile portfolios, long-short returns.
-# each month, rank tickers by momentum signal to find winner and loser, compute monthly W-L return
+'''
+daily rebalancing, quintile portfolios, long-short returns.
+each month, rank tickers by momentum signal to find winner and loser, compute monthly W-L return
+
+Pandas syntax
+df.reset_index().melet(): columns aggregated to single df column after groupby
+df.groupby(index)[column].transform(lambda func)
+    - transform adds qcut result to a column with the same index in flattened
+    - new column to host ticker's momentum signal score
+    - benefit over df.groupby(index)[column].apply(lambda func())
+        - applied = flattened.groupby('Date')['signal'].apply(lambda x: pd.qcut(x, 2, labels=False, duplicates='drop'))
+        - reduction of index handling after producing quantiles
+                                                   
+'''
 
 flattened = momentum_sgnl.reset_index().melt(
     id_vars='Date', 
-    var_name='ticker',   #columns aggregated to single column for groupby later
-    value_name='signal'  #new column to host ticker's momentum signal score
+    var_name='ticker',          
+    value_name='signal'         
     )
 return_align = return_monthl.shift(-1)
 flattened['return_aligned'] = return_align.reset_index().melt(id_vars='Date', var_name='ticker', value_name='return_aligned')['return_aligned']
 
-flattened['decile'] = flattened.groupby('Date')['signal'].transform( # transform adds qcut result to a column with the same index in flattened
+flattened['decile'] = flattened.groupby('Date')['signal'].transform( 
                                                 lambda x:pd.qcut(x,
                                                                  10, #10 quantiles
                                                                  labels=False, 
                                                                  duplicates='drop') )
 
-# additional index handling after producing quantiles
-# applied = flattened.groupby('Date')['signal'].apply(lambda x: pd.qcut(x, 2, labels=False, duplicates='drop'))
 top = flattened[flattened['decile']==9].groupby('Date')['return_aligned'].mean()
 bottom = flattened[flattened['decile']==0].groupby('Date')['return_aligned'].mean()
 
 long_short = top-bottom
 
 #Performance metrics
-#Sharpe ratio, max drawdown, annualised return, turnover
-#Plot cumulative returns, drawdown chart, rolling Sharpe.
-
+'''
+Sharpe ratio, max drawdown, annualised return, turnover
+Plot cumulative returns, drawdown chart, rolling Sharpe.
+'''
 sharpe = long_short.mean()/long_short.std() * np.sqrt(12)
 cumulative = (1+long_short).cumprod() # if log return, cumsum then exp?
 max_dd = (cumulative/cumulative.cummax()-1).min()
