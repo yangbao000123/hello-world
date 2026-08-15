fdf07458d0704b31910e8e55baf576040d4048ea
Yang Bao <93554411+yangbao328@users.noreply.github.com>
2026-08-15 16:21:24 -0500

    momentum visual

--- simulated_project/momentum_backtest.py
+++ simulated_project/momentum_backtest.py
@@ -10,6 +10,33 @@ import matplotlib.pyplot as plt
 import numpy as np
 from momentum_S5 import momentum
 
+
+def visualise_performance(strategy, cumulative, sharpe, feature, rolling_window=6):
+    
+    rolling_sharpe = strategy.rolling(rolling_window).mean() \
+                    /strategy.rolling(rolling_window).std() * np.sqrt(12)
+    
+    fig, ax1 = plt.subplots()
+    ax1.set_xlabel('Timestamp')
+    ax1.set_ylabel('Cumulative Return', color='lightpink')
+    ax1.plot(rolling_sharpe.index, cumulative, color='lightpink', linewidth=1.2)
+    ax1.tick_params(axis='y', labelcolor='lightpink')
+    ax1.tick_params(axis='x', rotation=45)
+    ax1.legend(['Cumulative Return'], loc='upper left')
+    
+    ax2 = ax1.twinx()
+    ax2.set_ylabel('Rolling Sharpe', color='lightseagreen')
+    ax2.axhline(sharpe, color='lightseagreen', linestyle='--', alpha=0.5)
+    ax2.plot(rolling_sharpe.index, rolling_sharpe, color='lightseagreen', linewidth=1.2)
+    ax2.tick_params(axis='y', labelcolor='lightseagreen')
+    ax2.tick_params(axis='x', rotation=45)
+    ax2.legend(['Rolling Sharpe'], loc='upper right')
+    
+    
+    fig.tight_layout()
+    plt.title(f'W minus L Performance, {feature}')
+    plt.show()
+
 url = 'https://raw.githubusercontent.com/datasets/s-and-p-500-companies/master/data/constituents.csv'
 start='2023-01-01'
 end='2026-07-30'
@@ -17,34 +44,16 @@ end='2026-07-30'
 momentum = momentum(start, end, url)  
 momentum_signal = momentum.generate_signal()
 #long_short, with_signal = momentum.backtest_momentum()
-strategy, with_signal = momentum.backtest_momentum()
-strategy_vol_target = momentum.volatility_targeting()
+strategy_plain, with_signal = momentum.backtest_momentum()
+metrics_plain, cumulative_plain = momentum.performance_evaluation(strategy_plain, VaR_threshold=.5)
+sharpe_plain = metrics_plain['Sharpe Ratio'].values
 
-metrics, cumulative = momentum.performance_evaluation(strategy_vol_target, VaR_threshold=.5)
-sharpe = metrics['Sharpe Ratio'].values
+
+strategy_vol_target = momentum.volatility_targeting()
+metrics_vol, cumulative_vol = momentum.performance_evaluation(strategy_vol_target, VaR_threshold=.5)
+sharpe_vol = metrics_vol['Sharpe Ratio'].values
 
 # Visualisation
-rolling_period = 6
-rolling_sharpe = strategy_vol_target.rolling(rolling_period).mean() \
-                /strategy_vol_target.rolling(rolling_period).std() * np.sqrt(12)
 
-fig, ax1 = plt.subplots()
-ax1.set_xlabel('Timestamp')
-ax1.set_ylabel('Cumulative Return', color='lightpink')
-ax1.plot(rolling_sharpe.index, cumulative, color='lightpink', linewidth=1.2)
-ax1.tick_params(axis='y', labelcolor='lightpink')
-ax1.tick_params(axis='x', rotation=45)
-ax1.legend(['Cumulative Return'], loc='upper left')
-
-ax2 = ax1.twinx()
-ax2.set_ylabel('Rolling Sharpe', color='lightseagreen')
-ax2.axhline(sharpe, color='lightseagreen', linestyle='--', alpha=0.5)
-ax2.plot(rolling_sharpe.index, rolling_sharpe, color='lightseagreen', linewidth=1.2)
-ax2.tick_params(axis='y', labelcolor='lightseagreen')
-ax2.tick_params(axis='x', rotation=45)
-ax2.legend(['Rolling Sharpe'], loc='upper right')
-
-
-fig.tight_layout()
-plt.title('W minus L Performance')
-plt.show()
+visualise_performance(strategy_vol_target, cumulative_vol, sharpe_vol, 'with volatility targeting')
+visualise_performance(strategy_plain, cumulative_plain, sharpe_plain, 'in plain')
